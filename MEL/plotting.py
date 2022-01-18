72a919f80a0a84489a3f54e7ec7530d6650b9496
Luna Pratali Maffei <luna.pratali@polimi.it>
2022-01-19 00:16:18 +0100

    pep8 formatting

--- MEL/plotting.py
+++ MEL/plotting.py
@@ -4,210 +4,224 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
+
 class PLOTTING:
-       '''
-       This function produces subplots of the profiles of the products at a given pressure
-       '''
-       def __init__(self,cwd):
-              '''
-              Checks if the output folder "PLOTS" exists and if not it creates it.
-              '''
-              self.path = cwd
-              self.path_plots = os.path.join(cwd,'PLOTS')
-              if os.path.exists(self.path_plots) == False:
-                     os.mkdir(self.path_plots)
+    '''
+    This function produces subplots of the profiles of the products at a given pressure
+    '''
 
+    def __init__(self, cwd):
+        '''
+        Checks if the output folder "PLOTS" exists and if not it creates it.
+        '''
+        self.path = cwd
+        self.path_plots = os.path.join(cwd, 'PLOTS')
+        if os.path.exists(self.path_plots) == False:
+            os.mkdir(self.path_plots)
 
-       def plot_data(self,profiles_Pi,REAC,PRODS,SPECIES,P):
-              '''
-              Extract the data common to each kind of plot
-              To be called at every new pressure
-              '''
-              # extract the T_VECT from the dictionary keys of profiles_Pi
-              T_LIST = list(profiles_Pi['detailed'].keys())
-              self.T_VECT = np.array(T_LIST,dtype=int)
-              # Define the number of rows and columns
-              if int(len(self.T_VECT)**0.5) == (len(self.T_VECT)**0.5):
-                     self.n_cols = int(len(self.T_VECT)**0.5)
-                     self.n_rows = self.n_cols
-              else:
-                     self.n_cols = int(len(self.T_VECT)**0.5)+1
-                     self.n_rows = int(len(self.T_VECT)**0.5)+int((len(self.T_VECT)/self.n_cols)>int(len(self.T_VECT)**0.5))
+    def plot_data(self, profiles_Pi, REAC, PRODS, SPECIES, P):
+        '''
+        Extract the data common to each kind of plot
+        To be called at every new pressure
+        '''
+        # extract the T_VECT from the dictionary keys of profiles_Pi
+        T_LIST = list(profiles_Pi['detailed'].keys())
+        self.T_VECT = np.array(T_LIST, dtype=int)
+        # Define the number of rows and columns
+        if int(len(self.T_VECT)**0.5) == (len(self.T_VECT)**0.5):
+            self.n_cols = int(len(self.T_VECT)**0.5)
+            self.n_rows = self.n_cols
+        else:
+            self.n_cols = int(len(self.T_VECT)**0.5)+1
+            self.n_rows = int(len(self.T_VECT)**0.5) + \
+                int((len(self.T_VECT)/self.n_cols) > int(len(self.T_VECT)**0.5))
 
-              self.profiles = profiles_Pi #dictionaries
-              self.REAC = REAC
-              self.SPECIES = SPECIES
-              self.PRODS = PRODS
-              self.P = P
+        self.profiles = profiles_Pi  # dictionaries
+        self.REAC = REAC
+        self.SPECIES = SPECIES
+        self.PRODS = PRODS
+        self.P = P
 
-              # linestyles for plotting
-              self.lstyles = ['-','--',':']
-              colorsrgb = list(matplotlib.cm.gist_rainbow(np.linspace(0,1,8)))
-              colorsrgb += list(matplotlib.cm.tab10(np.arange(0,10)))
-              colorsrgb += list(matplotlib.cm.Set3(np.arange(0,12)))
-              self.palette_series = pd.Series(colorsrgb[:len(SPECIES)],index=SPECIES)
-              self.palette_series[REAC] = 'black'
+        # linestyles for plotting
+        self.lstyles = ['-', '--', ':']
+        colorsrgb = list(matplotlib.cm.gist_rainbow(np.linspace(0, 1, 8)))
+        colorsrgb += list(matplotlib.cm.tab10(np.arange(0, 10)))
+        colorsrgb += list(matplotlib.cm.Set3(np.arange(0, 12)))
+        self.palette_series = pd.Series(
+            colorsrgb[:len(SPECIES)], index=SPECIES)
+        self.palette_series[REAC] = 'black'
 
-              # fontsizes
-              if len(self.T_VECT) > 9:
-                     self.fsize_vect = [6,7,8]
-              else:
-                     self.fsize_vect = [8,9,10]
-              # legend label size
-              if len(self.SPECIES) <= 10 and len(self.T_VECT) <= 9:
-                     self.legsize = 6
-              elif len(self.SPECIES) <= 10 and len(self.T_VECT) > 9:
-                     self.legsize = 5
-              elif len(self.SPECIES) > 10 and len(self.T_VECT) <= 9:
-                     self.legsize = 4
-              elif len(self.SPECIES) > 10 and len(self.T_VECT) > 9:
-                     self.legsize = 3
+        # fontsizes
+        if len(self.T_VECT) > 9:
+            self.fsize_vect = [6, 7, 8]
+        else:
+            self.fsize_vect = [8, 9, 10]
+        # legend label size
+        if len(self.SPECIES) <= 10 and len(self.T_VECT) <= 9:
+            self.legsize = 6
+        elif len(self.SPECIES) <= 10 and len(self.T_VECT) > 9:
+            self.legsize = 5
+        elif len(self.SPECIES) > 10 and len(self.T_VECT) <= 9:
+            self.legsize = 4
+        elif len(self.SPECIES) > 10 and len(self.T_VECT) > 9:
+            self.legsize = 3
 
+    def plot_exp_profiles(self, N_INIT):
+        '''
+        Produces profiles of the form Wi VS time
+        '''
+        # generate the figure
+        fig, axes = plt.subplots(self.n_rows, self.n_cols)
+        # set axis
+        row_plot = 0
+        col_plot = 0
+        Ti = 0
 
-       def plot_exp_profiles(self,N_INIT):
-              '''
-              Produces profiles of the form Wi VS time
-              '''
-              # generate the figure
-              fig, axes = plt.subplots(self.n_rows,self.n_cols)
-              # set axis
-              row_plot = 0
-              col_plot = 0
-              Ti = 0
-              
-              for T in self.T_VECT:
-                     Ti += 1
-                     # loop on the dictionaries in self.profiles
-                     kk = 0
-                     # set vect for maxt
-                     maxt = []
-                     # set axis: only col_plot if 1 row
-                     try:
-                            axx = axes[row_plot,col_plot]
-                     except IndexError:
-                            axx = axes[col_plot]
+        for T in self.T_VECT:
+            Ti += 1
+            # loop on the dictionaries in self.profiles
+            kk = 0
+            # set vect for maxt
+            maxt = []
+            # set axis: only col_plot if 1 row
+            try:
+                axx = axes[row_plot, col_plot]
+            except IndexError:
+                axx = axes[col_plot]
 
-                     for key in list(self.profiles.keys()):
-                            profiles_i = self.profiles[key]
-                            # extract the matrix with the profiles
-                            tW_DF = profiles_i[T]
-                            t = tW_DF['t'].values
-                            maxt.append(t[-1])
-                            t = t[:,np.newaxis]
-                            # plot the species only if present in the list
-                            for Si in self.SPECIES:
-                                   if np.where(tW_DF.columns==Si)[0].size > 0:                                                 
-                                          axx.plot(t,tW_DF[Si],color=self.palette_series[Si],ls=self.lstyles[kk])
+            for key in list(self.profiles.keys()):
+                profiles_i = self.profiles[key]
+                # extract the matrix with the profiles
+                tW_DF = profiles_i[T]
+                t = tW_DF['t'].values
+                maxt.append(t[-1])
+                t = t[:, np.newaxis]
+                # plot the species only if present in the list
+                for Si in self.SPECIES:
+                    if np.where(tW_DF.columns == Si)[0].size > 0:
+                        axx.plot(
+                            t, tW_DF[Si], color=self.palette_series[Si], ls=self.lstyles[kk])
 
-                            kk += 1
+                kk += 1
 
-                     # labels and formatting
-                     try:
-                            axes[self.n_rows-1,col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
-                            axes[row_plot,0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
-                     except IndexError:
-                            axes[col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
-                            axes[0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
-                     # if you don't have any plot below in the column: put the label
-                     if T == self.T_VECT[-1] and col_plot != self.n_cols-1:
-                            for miss_col in np.arange(col_plot+1,self.n_cols):
-                                   axes[self.n_rows-2,miss_col].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
-                     
-                     axx.set_title((str(T) + ' K'),fontsize=self.fsize_vect[2],fontweight='bold')
-                     axx.set_xlim([0,min(maxt)])
-                     axx.set_ylim([0,N_INIT])
-                     axx.axes.ticklabel_format(axis='both', style='sci', scilimits=(0,0),useLocale=True,useMathText=True)
-                     axx.yaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axx.xaxis.set_tick_params(labelsize=self.fsize_vect[0])
-                     axx.xaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axx.yaxis.set_tick_params(labelsize=self.fsize_vect[0])
-                     
-                     
-                     # update of the axis
-                     row_plot = row_plot + int((Ti%self.n_cols)==0) 
-                     col_plot = col_plot + 1 - self.n_cols*int((Ti%self.n_cols)==0) 
-      
-              try:
-                     axes[0,0].legend(list(self.SPECIES),loc=4,fontsize=self.legsize)
-                     if row_plot == self.n_rows-1:
-                            for cols in np.arange(col_plot,self.n_cols):
-                                   fig.delaxes(axes[-1,cols])  
-              except IndexError:
-                     axes[0].legend(list(self.SPECIES),loc=4,fontsize=self.legsize)
-              # set tight layout
-              fig.tight_layout(pad=0.1,w_pad=0.1)
-              # save the figure
-              fig_path = (self.path_plots + '/Prof_EXP_' + self.REAC + '_' + str(self.P) + '_atm.png')
-              if os.path.isfile(fig_path):
-                     os.remove(fig_path)
-              fig.savefig(fig_path,dpi=200)
-              
-              plt.close()
+            # labels and formatting
+            try:
+                axes[self.n_rows-1,
+                     col_plot].set_xlabel(r'$t$ [s]', fontsize=self.fsize_vect[1])
+                axes[row_plot, 0].set_ylabel(
+                    r'$X_{i}$ [-]', fontsize=self.fsize_vect[1])
+            except IndexError:
+                axes[col_plot].set_xlabel(
+                    r'$t$ [s]', fontsize=self.fsize_vect[1])
+                axes[0].set_ylabel(r'$X_{i}$ [-]', fontsize=self.fsize_vect[1])
+            # if you don't have any plot below in the column: put the label
+            if T == self.T_VECT[-1] and col_plot != self.n_cols-1:
+                for miss_col in np.arange(col_plot+1, self.n_cols):
+                    axes[self.n_rows-2,
+                         miss_col].set_xlabel(r'$t$ [s]', fontsize=self.fsize_vect[1])
 
+            axx.set_title((str(T) + ' K'),
+                          fontsize=self.fsize_vect[2], fontweight='bold')
+            axx.set_xlim([0, min(maxt)])
+            axx.set_ylim([0, N_INIT])
+            axx.axes.ticklabel_format(axis='both', style='sci', scilimits=(
+                0, 0), useLocale=True, useMathText=True)
+            axx.yaxis.offsetText.set_fontsize(self.fsize_vect[0])
+            axx.xaxis.set_tick_params(labelsize=self.fsize_vect[0])
+            axx.xaxis.offsetText.set_fontsize(self.fsize_vect[0])
+            axx.yaxis.set_tick_params(labelsize=self.fsize_vect[0])
 
-       def plot_data_reac(self,profiles_Pi,REAC,P,N_INIT_REAC):
-              '''
-              Plot the profiles of the reactant composition
-              '''
+            # update of the axis
+            row_plot = row_plot + int((Ti % self.n_cols) == 0)
+            col_plot = col_plot + 1 - self.n_cols*int((Ti % self.n_cols) == 0)
 
-              # generate the figure
-              fig,axes = plt.subplots(self.n_rows,self.n_cols)
-              # set axis
-              row_plot = 0
-              col_plot = 0
-              Ti = 0
+        try:
+            axes[0, 0].legend(list(self.SPECIES), loc=4, fontsize=self.legsize)
+            if row_plot == self.n_rows-1:
+                for cols in np.arange(col_plot, self.n_cols):
+                    fig.delaxes(axes[-1, cols])
+        except IndexError:
+            axes[0].legend(list(self.SPECIES), loc=4, fontsize=self.legsize)
+        # set tight layout
+        fig.tight_layout(pad=0.1, w_pad=0.1)
+        # save the figure
+        fig_path = (self.path_plots + '/Prof_EXP_' +
+                    self.REAC + '_' + str(self.P) + '_atm.png')
+        if os.path.isfile(fig_path):
+            os.remove(fig_path)
+        fig.savefig(fig_path, dpi=200)
 
-              for T in self.T_VECT:
-                     Ti += 1
-                     # extract the profiles
-                     tW_DF = profiles_Pi[T]
-                     t = tW_DF['t'].values
-                     t = t[:,np.newaxis]
-                     # redefine tW_DF based on the sum of the reactants mole fraction
-                     tW_DF_REAC_tot = np.sum(tW_DF[REAC].values,axis=1)
-                     tW_DF_REAC_tot = tW_DF_REAC_tot[:,np.newaxis]
-                     tW_DF[REAC] = tW_DF[REAC]/tW_DF_REAC_tot
-                     # plot the species
-                     # set axis: only col_plot if 1 row
-                     try:
-                            axx = axes[row_plot,col_plot]
-                     except IndexError:
-                            axx = axes[col_plot]
-                     axx.plot(t,tW_DF[REAC])      
-                     # labels and formatting
-                     try:
-                            axes[self.n_rows-1,col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
-                            axes[row_plot,0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
-                     except IndexError:
-                            axes[col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
-                            axes[0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
+        plt.close()
 
-                     axx.set_title((str(T) + ' K'),fontsize=self.fsize_vect[2],fontweight='bold')
-                     axx.set_ylim([0,np.max(np.max(tW_DF[REAC]))])
-                     axx.axes.ticklabel_format(axis='both', style='sci', scilimits=(0,0),useLocale=True,useMathText=True)
-                     axx.yaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axx.xaxis.set_tick_params(labelsize=self.fsize_vect[0])
-                     axx.xaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axx.yaxis.set_tick_params(labelsize=self.fsize_vect[0])
-                        
-                     # update of the axis
-                     row_plot = row_plot + int((Ti%self.n_cols)==0) 
-                     col_plot = col_plot + 1 - self.n_cols*int((Ti%self.n_cols)==0) 
+    def plot_data_reac(self, profiles_Pi, REAC, P, N_INIT_REAC):
+        '''
+        Plot the profiles of the reactant composition
+        '''
 
-              try:
-                     axes[0,0].legend(list(REAC),loc=4,fontsize=self.legsize)
-                     if row_plot == self.n_rows-1:
-                            for cols in np.arange(col_plot,self.n_cols):
-                                   fig.delaxes(axes[-1,cols])  
-              except IndexError:
-                     axes[0].legend(list(REAC),loc=4,fontsize=self.legsize)
+        # generate the figure
+        fig, axes = plt.subplots(self.n_rows, self.n_cols)
+        # set axis
+        row_plot = 0
+        col_plot = 0
+        Ti = 0
 
-              # set tight layout
-              fig.tight_layout(pad=0.1,w_pad=0.1)
-              # save the figure
-              fig_path = (os.path.join(self.path_plots, 'Prof_EXP_' + self.REAC + 'composition_' + str(self.P) + '_atm.png'))
-              if os.path.isfile(fig_path):
-                     os.remove(fig_path)
-              fig.savefig(fig_path,dpi=200)
-              
-              plt.close()
+        for T in self.T_VECT:
+            Ti += 1
+            # extract the profiles
+            tW_DF = profiles_Pi[T]
+            t = tW_DF['t'].values
+            t = t[:, np.newaxis]
+            # redefine tW_DF based on the sum of the reactants mole fraction
+            tW_DF_REAC_tot = np.sum(tW_DF[REAC].values, axis=1)
+            tW_DF_REAC_tot = tW_DF_REAC_tot[:, np.newaxis]
+            tW_DF[REAC] = tW_DF[REAC]/tW_DF_REAC_tot
+            # plot the species
+            # set axis: only col_plot if 1 row
+            try:
+                axx = axes[row_plot, col_plot]
+            except IndexError:
+                axx = axes[col_plot]
+            axx.plot(t, tW_DF[REAC])
+            # labels and formatting
+            try:
+                axes[self.n_rows-1,
+                     col_plot].set_xlabel(r'$t$ [s]', fontsize=self.fsize_vect[1])
+                axes[row_plot, 0].set_ylabel(
+                    r'$X_{i}$ [-]', fontsize=self.fsize_vect[1])
+            except IndexError:
+                axes[col_plot].set_xlabel(
+                    r'$t$ [s]', fontsize=self.fsize_vect[1])
+                axes[0].set_ylabel(r'$X_{i}$ [-]', fontsize=self.fsize_vect[1])
+
+            axx.set_title((str(T) + ' K'),
+                          fontsize=self.fsize_vect[2], fontweight='bold')
+            axx.set_ylim([0, np.max(np.max(tW_DF[REAC]))])
+            axx.axes.ticklabel_format(axis='both', style='sci', scilimits=(
+                0, 0), useLocale=True, useMathText=True)
+            axx.yaxis.offsetText.set_fontsize(self.fsize_vect[0])
+            axx.xaxis.set_tick_params(labelsize=self.fsize_vect[0])
+            axx.xaxis.offsetText.set_fontsize(self.fsize_vect[0])
+            axx.yaxis.set_tick_params(labelsize=self.fsize_vect[0])
+
+            # update of the axis
+            row_plot = row_plot + int((Ti % self.n_cols) == 0)
+            col_plot = col_plot + 1 - self.n_cols*int((Ti % self.n_cols) == 0)
+
+        try:
+            axes[0, 0].legend(list(REAC), loc=4, fontsize=self.legsize)
+            if row_plot == self.n_rows-1:
+                for cols in np.arange(col_plot, self.n_cols):
+                    fig.delaxes(axes[-1, cols])
+        except IndexError:
+            axes[0].legend(list(REAC), loc=4, fontsize=self.legsize)
+
+        # set tight layout
+        fig.tight_layout(pad=0.1, w_pad=0.1)
+        # save the figure
+        fig_path = (os.path.join(self.path_plots, 'Prof_EXP_' +
+                                 self.REAC + 'composition_' + str(self.P) + '_atm.png'))
+        if os.path.isfile(fig_path):
+            os.remove(fig_path)
+        fig.savefig(fig_path, dpi=200)
+
+        plt.close()
