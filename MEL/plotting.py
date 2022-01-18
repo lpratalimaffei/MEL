cc29600d10edaf46c7c14199549ec2ae33d41d36
Luna Pratali Maffei <luna.pratali@polimi.it>
2022-01-19 00:15:47 +0100

    bug fix: also 2 single T work

--- MEL/plotting.py
+++ MEL/plotting.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 
 class PLOTTING:
@@ -32,8 +33,8 @@ class PLOTTING:
               else:
                      self.n_cols = int(len(self.T_VECT)**0.5)+1
                      self.n_rows = int(len(self.T_VECT)**0.5)+int((len(self.T_VECT)/self.n_cols)>int(len(self.T_VECT)**0.5))
-              self.profiles = profiles_Pi #dictionaries
 
+              self.profiles = profiles_Pi #dictionaries
               self.REAC = REAC
               self.SPECIES = SPECIES
               self.PRODS = PRODS
@@ -41,12 +42,10 @@ class PLOTTING:
 
               # linestyles for plotting
               self.lstyles = ['-','--',':']
-              palette = plt.rcParams['axes.prop_cycle'].by_key()['color']
-              if len(palette) < len(SPECIES):
-                     ntot = round(len(SPECIES)/len(palette)) +1
-                     for i in np.arange(0,ntot):
-                            palette = palette + palette
-              self.palette_series = pd.Series(palette[0:len(SPECIES)],index=SPECIES)
+              colorsrgb = list(matplotlib.cm.gist_rainbow(np.linspace(0,1,8)))
+              colorsrgb += list(matplotlib.cm.tab10(np.arange(0,10)))
+              colorsrgb += list(matplotlib.cm.Set3(np.arange(0,12)))
+              self.palette_series = pd.Series(colorsrgb[:len(SPECIES)],index=SPECIES)
               self.palette_series[REAC] = 'black'
 
               # fontsizes
@@ -70,18 +69,24 @@ class PLOTTING:
               Produces profiles of the form Wi VS time
               '''
               # generate the figure
-              fig,axes = plt.subplots(self.n_rows,self.n_cols)
+              fig, axes = plt.subplots(self.n_rows,self.n_cols)
               # set axis
               row_plot = 0
               col_plot = 0
               Ti = 0
-
+              
               for T in self.T_VECT:
                      Ti += 1
                      # loop on the dictionaries in self.profiles
                      kk = 0
                      # set vect for maxt
                      maxt = []
+                     # set axis: only col_plot if 1 row
+                     try:
+                            axx = axes[row_plot,col_plot]
+                     except IndexError:
+                            axx = axes[col_plot]
+
                      for key in list(self.profiles.keys()):
                             profiles_i = self.profiles[key]
                             # extract the matrix with the profiles
@@ -91,38 +96,44 @@ class PLOTTING:
                             t = t[:,np.newaxis]
                             # plot the species only if present in the list
                             for Si in self.SPECIES:
-                                   if np.where(tW_DF.columns==Si)[0].size > 0:
-                                          axes[row_plot,col_plot].plot(t,tW_DF[Si],color=self.palette_series[Si],ls=self.lstyles[kk])      
+                                   if np.where(tW_DF.columns==Si)[0].size > 0:                                                 
+                                          axx.plot(t,tW_DF[Si],color=self.palette_series[Si],ls=self.lstyles[kk])
+
                             kk += 1
 
                      # labels and formatting
-                     axes[self.n_rows-1,col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
+                     try:
+                            axes[self.n_rows-1,col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
+                            axes[row_plot,0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
+                     except IndexError:
+                            axes[col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
+                            axes[0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
                      # if you don't have any plot below in the column: put the label
-                     #miss_plots = (self.n_rows*self.n_cols)-len(self.T_VECT)
-                     #miss_col = np.arange(self.n_cols-miss_plots,self.n_cols)
                      if T == self.T_VECT[-1] and col_plot != self.n_cols-1:
                             for miss_col in np.arange(col_plot+1,self.n_cols):
                                    axes[self.n_rows-2,miss_col].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
-
-                     axes[row_plot,0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
-                     axes[row_plot,col_plot].set_title((str(T) + ' K'),fontsize=self.fsize_vect[2],fontweight='bold')
-                     axes[row_plot,col_plot].set_xlim([0,min(maxt)])
-                     axes[row_plot,col_plot].set_ylim([0,N_INIT])
-                     axes[row_plot,col_plot].axes.ticklabel_format(axis='both', style='sci', scilimits=(0,0),useLocale=True,useMathText=True)
-                     axes[row_plot,col_plot].yaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axes[row_plot,col_plot].xaxis.set_tick_params(labelsize=self.fsize_vect[0])
-                     axes[row_plot,col_plot].xaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axes[row_plot,col_plot].yaxis.set_tick_params(labelsize=self.fsize_vect[0])
+                     
+                     axx.set_title((str(T) + ' K'),fontsize=self.fsize_vect[2],fontweight='bold')
+                     axx.set_xlim([0,min(maxt)])
+                     axx.set_ylim([0,N_INIT])
+                     axx.axes.ticklabel_format(axis='both', style='sci', scilimits=(0,0),useLocale=True,useMathText=True)
+                     axx.yaxis.offsetText.set_fontsize(self.fsize_vect[0])
+                     axx.xaxis.set_tick_params(labelsize=self.fsize_vect[0])
+                     axx.xaxis.offsetText.set_fontsize(self.fsize_vect[0])
+                     axx.yaxis.set_tick_params(labelsize=self.fsize_vect[0])
                      
                      
                      # update of the axis
                      row_plot = row_plot + int((Ti%self.n_cols)==0) 
                      col_plot = col_plot + 1 - self.n_cols*int((Ti%self.n_cols)==0) 
-
-              if row_plot == self.n_rows-1:
-                     for cols in np.arange(col_plot,self.n_cols):
-                            fig.delaxes(axes[-1,cols])        
-              axes[0,0].legend(list(self.SPECIES),loc=4,fontsize=self.legsize)
+      
+              try:
+                     axes[0,0].legend(list(self.SPECIES),loc=4,fontsize=self.legsize)
+                     if row_plot == self.n_rows-1:
+                            for cols in np.arange(col_plot,self.n_cols):
+                                   fig.delaxes(axes[-1,cols])  
+              except IndexError:
+                     axes[0].legend(list(self.SPECIES),loc=4,fontsize=self.legsize)
               # set tight layout
               fig.tight_layout(pad=0.1,w_pad=0.1)
               # save the figure
@@ -157,26 +168,40 @@ class PLOTTING:
                      tW_DF_REAC_tot = tW_DF_REAC_tot[:,np.newaxis]
                      tW_DF[REAC] = tW_DF[REAC]/tW_DF_REAC_tot
                      # plot the species
-                     axes[row_plot,col_plot].plot(t,tW_DF[REAC])      
+                     # set axis: only col_plot if 1 row
+                     try:
+                            axx = axes[row_plot,col_plot]
+                     except IndexError:
+                            axx = axes[col_plot]
+                     axx.plot(t,tW_DF[REAC])      
                      # labels and formatting
-                     axes[self.n_rows-1,col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
-                     axes[row_plot,0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
-                     axes[row_plot,col_plot].set_title((str(T) + ' K'),fontsize=self.fsize_vect[2],fontweight='bold')
-                     axes[row_plot,col_plot].set_ylim([0,np.max(np.max(tW_DF[REAC]))])
-                     axes[row_plot,col_plot].axes.ticklabel_format(axis='both', style='sci', scilimits=(0,0),useLocale=True,useMathText=True)
-                     axes[row_plot,col_plot].yaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axes[row_plot,col_plot].xaxis.set_tick_params(labelsize=self.fsize_vect[0])
-                     axes[row_plot,col_plot].xaxis.offsetText.set_fontsize(self.fsize_vect[0])
-                     axes[row_plot,col_plot].yaxis.set_tick_params(labelsize=self.fsize_vect[0])
+                     try:
+                            axes[self.n_rows-1,col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
+                            axes[row_plot,0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
+                     except IndexError:
+                            axes[col_plot].set_xlabel(r'$t$ [s]',fontsize=self.fsize_vect[1])
+                            axes[0].set_ylabel(r'$X_{i}$ [-]',fontsize=self.fsize_vect[1])
+
+                     axx.set_title((str(T) + ' K'),fontsize=self.fsize_vect[2],fontweight='bold')
+                     axx.set_ylim([0,np.max(np.max(tW_DF[REAC]))])
+                     axx.axes.ticklabel_format(axis='both', style='sci', scilimits=(0,0),useLocale=True,useMathText=True)
+                     axx.yaxis.offsetText.set_fontsize(self.fsize_vect[0])
+                     axx.xaxis.set_tick_params(labelsize=self.fsize_vect[0])
+                     axx.xaxis.offsetText.set_fontsize(self.fsize_vect[0])
+                     axx.yaxis.set_tick_params(labelsize=self.fsize_vect[0])
                         
                      # update of the axis
                      row_plot = row_plot + int((Ti%self.n_cols)==0) 
                      col_plot = col_plot + 1 - self.n_cols*int((Ti%self.n_cols)==0) 
 
-              if row_plot == self.n_rows-1:
-                     for cols in np.arange(col_plot,self.n_cols):
-                            fig.delaxes(axes[-1,cols])        
-              axes[0,0].legend(list(REAC),loc=4,fontsize=self.legsize)
+              try:
+                     axes[0,0].legend(list(REAC),loc=4,fontsize=self.legsize)
+                     if row_plot == self.n_rows-1:
+                            for cols in np.arange(col_plot,self.n_cols):
+                                   fig.delaxes(axes[-1,cols])  
+              except IndexError:
+                     axes[0].legend(list(REAC),loc=4,fontsize=self.legsize)
+
               # set tight layout
               fig.tight_layout(pad=0.1,w_pad=0.1)
               # save the figure
